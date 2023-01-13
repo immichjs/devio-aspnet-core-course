e3edb218e2a2b7ec6eca82f511d7f06b29f2ba7f
Michel França <fr.mich17@gmail.com>
2023-01-13 02:09:06 -0300

    Curso inicial de ASP.NET Core concluído

---
+++ MeuApp/MeuMiddleware.cs
@@ -0,0 +1,28 @@
+﻿namespace MeuApp
+{
+    public class MeuMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public MeuMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine("\n\r ---- Antes ---- \n\r");
+
+            await _next(context);
+
+            Console.WriteLine("\n\r ---- Depois ---- \n\r");
+        }
+    }
+    
+    public static class MeuMiddlewareExtension
+    {
+        public static IApplicationBuilder UseMeMiddleware(this IApplicationBuilder builder) {
+            return builder.UseMiddleware<MeuMiddleware>();
+        }
+    }
+}
